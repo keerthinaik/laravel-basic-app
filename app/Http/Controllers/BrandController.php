52f73550a5da22373798e328b241130523f0b7db
KEERTHINAIK R <keerthinaikr1996@gmail.com>
2021-05-20 14:59:57 +0530

    added image resize and upload functionality for Brand model

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
 
 class BrandController extends Controller
 {
     public function all_brand()
     {
-        $brands = Brand::latest()->paginate(2);
+        $brands = Brand::latest()->paginate(3);
         return view('admin.brand.index', compact('brands'));
     }
 
@@ -29,7 +31,12 @@ class BrandController extends Controller
         $image_ext = strtolower($image->getClientOriginalExtension());
         $image_name = $name_gen . '.' . $image_ext;
         $upload_dir = 'images/brands/';
-        $image->move($upload_dir, $image_name);
+
+        // resizing and saving the image
+        Image::make($image)->resize(300, 200)->save($upload_dir . $image_name);
+
+        // normal image inserting
+        // $image->move($upload_dir, $image_name);
 
         $brand = new Brand();
         $brand->name = $request->name;
